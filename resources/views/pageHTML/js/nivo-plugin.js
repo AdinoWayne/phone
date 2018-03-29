d99ed0ed2cd0ffb0f115fa8247596e1ab2fefb81
= <=>
2018-03-29 17:12:22 +0700

    29/3/2018
    front-end

---
+++ resources/views/pageHTML/js/nivo-plugin.js
@@ -0,0 +1,15 @@
+(function ($) {
+    "use strict";
+	/*----- main slider -----*/
+	$('#mainSlider').nivoSlider({
+		directionNav: true,
+		animSpeed: 500,
+		slices: 18,
+		pauseTime: 5000,
+		pauseOnHover: false,
+		controlNav: false,
+		prevText: '<i class="fa fa-angle-left nivo-prev-icon"></i>',
+		nextText: '<i class="fa fa-angle-right nivo-next-icon"></i>'
+	});
+	
+})(jQuery);
